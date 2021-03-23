99024e6193f4b81368df1442e6d56f0b54eb1696
Manny Jois <3893724+mkjois@users.noreply.github.com>
2021-03-23 22:35:40 +0000

    More succinct bash script template

--- macosx/files/lib/script-template-bash.sh
+++ macosx/files/lib/script-template-bash.sh
@@ -1,29 +1,6 @@
 #!/usr/bin/env bash
 
 set -ETeuo pipefail
-
-### ANY PRE-MAIN SETUP GOES HERE
-
-true
-
-### ANY POST-MAIN OR ON-ERROR CLEANUP GOES HERE
-
-err_exit() {
-    echo -e "$2" >&2
-    exit $1
-}
-
-on_exit() {
-    exit_code=$?
-    if test ${exit_code} -eq 0; then
-        true
-    else
-        echo -e '\nFailed' >&2
-    fi
-}
-
+err_exit() { echo -e "$2" >&2; exit $1; }
+on_exit() { exit_code=$?; if test ${exit_code} -ne 0; then echo -e '\nFailed' >&2; fi; }
 trap on_exit EXIT
-
-### MAIN CODE GOES HERE
-
-false
